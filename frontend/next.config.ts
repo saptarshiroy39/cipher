8abd9f6c5ed918f261497f6c992634e139d11eb3
Saptarshi Roy <saptarshiroy39@gmail.com>
2026-06-11 10:43:42 +0530

    Update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactCompiler: true,
+  allowedDevOrigins: [
+    'localhost:3000',                   // Local development host
+    '*.lhr.life',                       // localhost.run secure tunnels
+    '*.localhost.run',                  // localhost.run alternative domains
+    '*.ngrok-free.app',                 // Ngrok free subdomains
+    '*.preview.app.github.dev',         // Github Codespaces tunnel domains
+    '*.tunnels.api.visualstudio.com',   // VS Code native port forwarding tunnels
+  ],
 };
 
 export default nextConfig;
